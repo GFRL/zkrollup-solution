25f0129d72252acd47aaba1e3ee4f49fa094aa1d
GFRL <2200013213@stu.pku.edu.cn>
2024-02-22 19:12:08 +0800

    Add the fourth
    增加了多zk的第二种情况

--- Code/python/main.py
+++ Code/python/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-MAXX=100
+MAXX=98
 numm=1000
 #solve_second
 #input( A, B, C)
@@ -227,10 +227,10 @@ def compute1(type=0):
             else:
                 Lpercent.append(50*E2/E1)
 
-def compute(type=0):
+def compute3(type=0):
     if type == 0 :#足够多钱
-        X1 = solve_second((N + 2) * (N + 2), 2 * C * (N + 2) - (N + 1) * S,  - S * C + M)
-        X2 = solve_second((N + 1) * (N + 1), 2 * C * (N + 1) - N * S, - S * C + M)
+        X1 = solve_second((N + 2) * (N + 2), 2 * C * (N + 2) - (N + 1) * S,  C*C- S * C + M)
+        X2 = solve_second((N + 1) * (N + 1), 2 * C * (N + 1) - N * S, C*C- S * C + M)
         E1 = ((M / X1) + S) * X1 / ((N + 2) * X1 + C) - X1#平衡点运算的时候进行了变形，X_i实际上是X_i-b;
         E2 = ((M / X2) + S) * X2 / ((N + 1) * X2 + C) - X2
         #if (E2 < 0):
@@ -269,21 +269,21 @@ def compute(type=0):
     elif type == 1 :
         X1 = C_suppose
         X2 = 2 * C_suppose
-        X11 = solve_second((N + 2) * (N + 2), 2 * C * (N + 2) - (N + 1) * S, -S * C + M)
+        X11 = solve_second((N + 2) * (N + 2), 2 * C * (N + 2) - (N + 1) * S, C*C-S * C + M)
 
-        X22 = solve_second((N + 1) * (N + 1), 2 * C * (N + 1) - N * S, -S * C + M)
+        X22 = solve_second((N + 1) * (N + 1), 2 * C * (N + 1) - N * S, C*C-S * C + M)
         if (X1 > X11) :
             X1 = X11 
           #  return
         else:
             X11 = solve_second(N * N, 2 * (C + 2 * X1) * N - (N - 1) * S,
-				 - S * (C + 2 * X1) + M)
+				 (C + 2 * X1)*(C + 2 * X1)- S * (C + 2 * X1) + M)
         if (X2 > X22) :
             X2 = X22 
            # return
         else:
             X22= solve_second(N * N, 2 * (C + X2) * N - (N - 1) * S,
-				 - S * (C + X2) + M)
+				 (C + X2)*(C + X2)- S * (C + X2) + M)
         E1 = (M  + S * X1) / (N * X11 + C + 2 * X1) - X1 
         E2 = (M  + S* X2) / (N * X22 + C + X2) - X2
         if(E1<=0):
@@ -305,10 +305,51 @@ def compute(type=0):
             # print(E22)
         #if (E2 < 0):   
             #return
+        print("C="+str(C)+" and X1="+str(X1)+" and x2="+str(X2)+" and x11="+str(X11)+" and x22="+str(X22)+" and E1="+str(E1)+" and E2="+str(E2))
         LX1.append(X1)
         LX2.append(X2)
         LX11.append(X11)
         LX22.append(X22)
+        LE1.append(E1)
+        LE2.append(E2)
+        if(X1==0):
+            Lpercent1.append(100)
+        else:
+            Lpercent1.append(E1*100/X1)
+        if(X2==0):
+            Lpercent2.append(100)
+        else:
+            Lpercent2.append(E2*100/X2)
+        if(X1 ==0 ):
+            if(X2==0):    
+                Lpercent.append(100)
+            else:
+                Lpercent.append(200)
+        else:
+            if(50*E2/E1>200):
+                Lpercent.append(200)
+            else:
+                Lpercent.append(50*E2/E1)
+
+def compute(type=0):
+    if type == 0 :#足够多钱
+        X1 = solve_second((N+2)*(N+2)+(2*N+3)*S, 2 * C * (N + 2) - (N + 1) * M+2*S*C,  C*C-C*M)
+        X2 = solve_second((N+1)*(N+1)+(2*N+1)*S, 2 * C * (N + 1) - (N) * M+2*S*C,  C*C-C*M)
+        E1 = (M - S*X1) * X1 / ((N + 2) * X1 + C) - X1#平衡点运算的时候进行了变形，X_i实际上是X_i-b;
+        E2 = (M - S*X2) * X2 / ((N + 1) * X2 + C) - X2
+        #if (E2 < 0):
+         #   return
+        #print("M=%.0lf,C=%.0lf(%.0lf),b=%.0lf\n", M, C, C / C_average, b)
+        if(E1<=0):
+            X1=0
+            E1 = 0
+            X11=0
+        if(E2<=0):
+            X2=0
+            E2 =0
+            X22=0
+        LX1.append(X1)
+        LX2.append(X2)
         LE1.append(E1)
         LE2.append(E2)
         if(X1==0):
@@ -329,10 +370,76 @@ def compute(type=0):
                 Lpercent.append(200)
             else:
                 Lpercent.append(50*E2/E1)
-
-#b = 1; C = 19 * 98; C_suppose = 20; total_limit = 3000; C_average = 19;M = 2040
+    elif type == 1 :
+        X1 = C_suppose
+        X2 = 2 * C_suppose
+        X11 = solve_second((N+2)*(N+2)+(2*N+3)*S, 2 * C * (N + 2) - (N + 1) * M+2*S*C,  C*C-C*M)
+        X22 = solve_second((N+1)*(N+1)+(2*N+1)*S, 2 * C * (N + 1) - (N) * M+2*S*C,  C*C-C*M)
+        if (X1 > X11) :
+            X1 = X11 
+          #  return
+        else:
+            X11 = solve_second(N*N+(2*N-1)*S, 2 * (C+2*X1) * (N) - (N - 1) * M+2*S*(C+2*X1), 
+                                (C+2*X1)*(C+2*X1)-(C+2*X1)*M)
+        if (X2 > X22) :
+            X2 = X22 
+           # return
+        else:
+            X22= solve_second(N*N+(2*N-1)*S, 2 * (C+X2) * (N) - (N - 1) * M+2*S*(C+X2), 
+                                (C+X2)*(C+X2)-(C+X2)*M)
+        E1 = (M  - S * X1)*X1 / (N * X11 + C + 2 * X1) - X1 
+        E2 = (M  - S* X2) *X2/ (N * X22 + C + X2) - X2
+        if(E1<=0):
+            X1=0
+            E1 = 0
+            X11=0
+        if(E2<=0):
+            X2=0
+            E2 =0
+            X22=0
+            # print("WHy! ",end="")
+            # print("E1= ",end="")
+            # print(E1,end="")
+            # print("E2= ",end="")
+            # print(E2,end="")
+            # print("E11= ",end="")
+            # print(E11,end="")
+            # print("E22= ",end="")
+            # print(E22)
+        #if (E2 < 0):   
+            #return
+        print("C="+str(C)+" and X1="+str(X1)+" and x2="+str(X2)+" and x11="+str(X11)+" and x22="+str(X22)+" and E1="+str(E1)+" and E2="+str(E2))
+        LX1.append(X1)
+        LX2.append(X2)
+        LX11.append(X11)
+        LX22.append(X22)
+        LE1.append(E1)
+        LE2.append(E2)
+        if(X1==0):
+            Lpercent1.append(100)
+        else:
+            Lpercent1.append(E1*100/X1)
+        if(X2==0):
+            Lpercent2.append(100)
+        else:
+            Lpercent2.append(E2*100/X2)
+        if(X1 ==0 ):
+            if(X2==0):    
+                Lpercent.append(100)
+            else:
+                Lpercent.append(200)
+        else:
+            if(50*E2/E1>200):
+                Lpercent.append(200)
+            else:
+                Lpercent.append(50*E2/E1)
 #b = 1; C = 19 * 98; C_suppose = 20; total_limit = 3000;S = 2100; M = 900
-b=1;C=20*98;C_suppose=20;total_limit=3000;S=400;M=20*(40800/19-S)
+                
+#b = 1; C = 19 * 98; C_suppose = 20; total_limit = 3000; C_average = 19;M = 2040
+
+#b=1;C=20*98;C_suppose=20;total_limit=3000;S=2000;M=20*(40800/19-S)
+
+b=1;C=20*98;C_suppose=20;total_limit=3000;S=0.1;M=(40800/19-20*S)
 mycolor=["b","c","g","k","m","r","y"]
 print("MX/(X-b)  very rich\n")
 plt.figure(1)
@@ -358,7 +465,7 @@ plt.subplot(1,2,2)
 plt.xlabel("Number of Other clever people")
 plt.ylabel("benifit percent of E2")
 for j in range(8):
-    C_suppose=11+j
+    C_suppose=5*(j+1)
     clear()
     for i in LN:
         N = i; C = (98 - i) * 19
@@ -400,23 +507,23 @@ plt.plot(LN,Lpercent2,color='b',label="C_suppose1="+str(C_suppose))
 
 
 plt.figure(1)
-plt.savefig("./new example3_detail tempetation.png")
+plt.savefig("./new example4_detail tempetation.png")
 
 plt.figure(2)
-plt.savefig("./new example3_detail benifit.png")
+plt.savefig("./new example4_detail benifit.png")
 
 plt.figure(3)
-plt.savefig("./new example3_detail benifit percent.png")
+plt.savefig("./new example4_detail benifit percent.png")
 
 
 # plt.figure(1)
-# plt.savefig("./new example3_simple tempetation.png")
+# plt.savefig("./new example4_simple tempetation.png")
 
 # plt.figure(2)
-# plt.savefig("./new example3_simple benifit.png")
+# plt.savefig("./new example4_simple benifit.png")
 
 # plt.figure(3)
-# plt.savefig("./new example3_simple benifit percent.png")
+# plt.savefig("./new example4_simple benifit percent.png")
 
 
 # plt.figure(4)
